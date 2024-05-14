02420c1d461da9d648cc9829b417c2a2972b2078
Ethan H <12magoo19@gmail.com>
2024-05-13 18:53:19 -0700

    Update sniff_sched.sh

--- Project4/sniff_sched.sh
+++ Project4/sniff_sched.sh
@@ -1,4 +1,5 @@
-# Sniff Script to start a packet
+# Sniff Script to start a packet Search
+#Scheduled to run for 5 minutes
 
 tcpdump -G 300 -W 1 -i wlan0 -w sniff_logs.pcap -Z root
 # -G (seconds) -i (network) -w (filename)
